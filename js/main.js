a61ab0b2f4cd4a31e7de35bed0042ff72e32ec8e
Иван Турьев <onlyfl@ukr.net>
2017-03-30 16:48:20 +0300

    first

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(function() {
+    // code..
+})
+
+$(window).load(function(){
+				$('.slider').flexslider({
+					 animation: "slide",
+					
+					directionNav: false,
+					controlNav: true
+				
+						
+					});
+					});
+					
+					
